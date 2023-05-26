de3346850cbcb1bdf58416e75a00c6feb8ef8902
l3on <l3onwebdev@gmail.com>
2023-05-26 16:44:05 +0200

    added Html basic structure

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 /*
-Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-il prezzo del biglietto è definito in base ai km (0.21 € al km)
-va applicato uno sconto del 20% per i minorenni
-va applicato uno sconto del 40% per gli over 65.
+Bonus
+Stampare il prezzo finale sulla pagina invece che in console
+Aggiungere, sempre in pagina, altre informazioni, come il prezzo prima dello sconto e l'età e il chilometraggio indicati dall'utente
+Validazione
+Abbellimenti vari con CSS
 */
 
 /* *****************************************
