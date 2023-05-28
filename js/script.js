ee072f10b0d4ed566ca15e1f38d546c426ae84db
l3on <l3onwebdev@gmail.com>
2023-05-28 11:18:08 +0200

    fixed JS comments

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 /* *****************************************
 ! DEBUG
 * *****************************************/
-// Check if file is linked to the HTML
-console.log('JS OK!')
+console.log('=============== DEBUG =================');
+
+console.log('JS OK!');// Check if JS is linked to the page
 
 
 /* *****************************************
@@ -11,24 +12,24 @@ console.log('JS OK!')
 console.log('=============== INIT =================');
 
 
-//###### Ticket Data ######\\
-// Ticket price per KM
-const pricePerKm = 0.21;
+// *** PRICE DATA ***//
+const pricePerKm = 0.21;// Ticket price per KM [€]
 
-// Price Reductions
-const underAgeValue = 18;
-const underAgeDiscount = 0.20;
+// Underage Price Reduction
+const underAgeValue = 18;// [years]
+const underAgeDiscount = 0.20;// [percentage unit]
 
-const overAgeValue = 65;
-const overAgeDiscount = 0.40;
+// Overage Price Reduction
+const overAgeValue = 65;// [years]
+const overAgeDiscount = 0.40;// [percentage unit]
 
-// Log Data
+//! Log Data
 console.log('Prezzo del biglietto per KM: €' + pricePerKm);
 console.log(`Sconto Minorenni: Sconto del ${underAgeDiscount.toFixed(2)}% se hai un età inferiore a ${underAgeValue} anni.`);
 console.log(`Sconto "Over 65": Sconto del ${overAgeDiscount.toFixed(2)}% se hai un età superiore o uguale a ${overAgeValue} anni.`);
 
 
-//###### DOM Elements ######\\
+// *** GET DOM ELEMENTS ***//
 const tripLengthElem = document.getElementById('trip-length');
 const passengerAgeElem = document.getElementById('passenger-age');
 const ticketKmElem = document.getElementById('ticket-km-price');
@@ -36,7 +37,7 @@ const ticketBaseElem = document.getElementById('ticket-base-price');
 const discountElem = document.getElementById('discount');
 const ticketFinalPriceElem = document.getElementById('ticket-final-price');
 
-// Log
+//! Log Elements
 console.log('=============== Elementi DOM =================');
 console.log('Trip Length Element:', tripLengthElem);
 console.log('Passenger Age Element:', passengerAgeElem);
@@ -52,93 +53,91 @@ console.log('Ticket Price Element:', ticketFinalPriceElem);
 console.log('=============== LOGIC =================');
 
 
-//###### Retrieve user data ######\\
+// *** GET USER INPUTS ***//
 // Get trip length
 const tripLengthInput = parseInt(prompt('Fornisci la lunghezza del viaggio in KM', 10));
 
 // Get passenger age
 const passengerAgeInput = parseInt(prompt('Fornisci l\'età del passeggero', 8));
 
-// Log data
+//! Log Inputs
 console.log(`Lunghezza viaggio: ${tripLengthInput} KM.`)
 console.log(`Età passeggero: ${passengerAgeInput} anni.`)
 
 
-//###### Validation ######\\
+// *** VALIDATION ***//
 let isValid = true;
-let errorMsg = '';
-const errorElem = '<span class="text-danger">inserisci un numero maggiore di 0!</span>';
+let errorMsg = '';// Message container for error logs
+const errorElem = '<span class="text-danger">inserisci un numero maggiore di 0!</span>';// Element to insert on page if an input is invalid
 
-// Trip length input check
+// Check trip length input
 if(isNaN(tripLengthInput) || tripLengthInput < 1) {
 
     isValid = false;
 
     // Update error log message
-    errorMsg += 'Lunghezza viaggio non valido: inserisci un numero maggiore di 0!\n';
+    errorMsg += '### Lunghezza viaggio non valido: inserisci un numero maggiore di 0!\n';
 
     // Update DOM element
     tripLengthElem.innerHTML = errorElem;
 }
 
-// Age input check
+// Check age input 
 if(isNaN(passengerAgeInput) || passengerAgeInput < 1) {
+
     isValid = false;
 
-    // Create error message
-    const msg = 'Anni passeggero non validi: inserisci un numero maggiore di 0!';
-
     // Update error log message
-    errorMsg += 'Anni passeggero non validi: inserisci un numero maggiore di 0!\n';
+    errorMsg += '### Anni passeggero non validi: inserisci un numero maggiore di 0!\n';
 
     // Update DOM element
     passengerAgeElem.innerHTML = errorElem;
 }
 
 
+// *** UPDATE DATA ***//
 if(!isValid) {
-    //###### Log error ######\\
+
+    //! Log error
     console.log(errorMsg);
 
 } else {
 
-    //###### Calculate standard ticket price ######\\
+    // *** CALCULATE FINAL PRICE ***//
+    // Calculate standard ticket price
     const ticketBasePrice = tripLengthInput * pricePerKm;
 
-    // Log result
+    //! Log standard price
     console.log('Prezzo biglietto standard: €' + ticketBasePrice.toFixed(2));
 
 
-    //###### Calculate ticket reductions ######\\
-    // Final price var
-    let ticketFinalPrice = ticketBasePrice;
+    // Apply ticket reductions
+    let ticketFinalPrice = ticketBasePrice;// Final price
+    let discountMsg = 'Nessuno';// Discount message
 
-    // Discount string
-    let discountMsg = 'Nessuno';
-
-    // Under age check
+    // Underage check
     if (passengerAgeInput < underAgeValue) {
 
-        ticketFinalPrice *= (1 - underAgeDiscount);
-        discountMsg = `"Minorenni" del ${underAgeDiscount * 100}%`;
+        ticketFinalPrice *= (1 - underAgeDiscount);// Apply ticket reduction
+        discountMsg = `"Minorenni" del ${underAgeDiscount * 100}%`;// Create discount message
 
     } 
-    // Over age check
+    // Overage check
     else if (passengerAgeInput >= overAgeValue) {
 
-        ticketFinalPrice *= (1 - overAgeDiscount);
-        discountMsg = `"Over 65" del ${overAgeDiscount * 100}%`;
+        ticketFinalPrice *= (1 - overAgeDiscount);// Apply ticket reduction
+        discountMsg = `"Over 65" del ${overAgeDiscount * 100}%`;// Create discount message
     }
 
-    // Log discount
+    //! Log discount message
     console.log('Sconto applicato: ' + discountMsg);
 
 
-    //###### Log final price ######\\
+    // *** RENDER RESULTS ***//
+    //! Log final price
     console.log('Il prezzo finale del biglietto è di €' + ticketFinalPrice.toFixed(2));
 
-
-    //###### Update DOM elements ######\\
+    // Update DOM elements
     tripLengthElem.innerText = tripLengthInput + ' KM';
     passengerAgeElem.innerText = passengerAgeInput + ' Anni';
     ticketKmElem.innerText = '€' + pricePerKm.toFixed(2) + ' per KM';
@@ -147,5 +146,6 @@ if(!isValid) {
     ticketFinalPriceElem.innerText = '€' + ticketFinalPrice.toFixed(2);
 
 }
+
 
 console.log('=============== DONE =================');
